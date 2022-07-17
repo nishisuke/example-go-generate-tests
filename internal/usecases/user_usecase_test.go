6978e453d2e46a714ffae60c4ee4d75de2dd90ee
nishisuke <ryoskns+git@gmail.com>
2022-07-17 13:58:39 +0900

    Revert "Re-generate test"
    This reverts commit 03146aceab105681d15b81f64eed6a3886723710.

--- internal/usecases/user_usecase_test.go
+++ internal/usecases/user_usecase_test.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"reflect"
 	"sample/internal/models"
+	"sample/internal/usecases/mock_usecases"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 )
 
 func TestUserUsecase_GetMe(t *testing.T) {
+	validUser := models.User{Name: "Valid"}
 	type fields struct {
 		userRepo UserRepo
 	}
@@ -23,7 +25,17 @@ func TestUserUsecase_GetMe(t *testing.T) {
 		want       models.User
 		wantErr    error
 	}{
-		// TODO: Add test cases.
+		{
+			"case1",
+			func(ctrl *gomock.Controller) UserUsecase {
+				m := mock_usecases.NewMockUserRepo(ctrl)
+				m.EXPECT().Find(validUser.ID).Return(validUser)
+				return NewMockUserUsecase(m)
+			},
+			args{validUser.ID},
+			validUser,
+			nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
